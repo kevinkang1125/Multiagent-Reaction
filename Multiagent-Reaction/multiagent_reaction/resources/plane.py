9716b3ff4100126a88bfeb23d655aa7d956e9f15
kevinkang <1361788628@qq.com>
2022-09-18 19:49:03 +0800

    model fix

--- Multiagent-Reaction/multiagent_reaction/resources/plane.py
+++ Multiagent-Reaction/multiagent_reaction/resources/plane.py
@@ -4,8 +4,8 @@ import os
 
 class Plane:
     def __init__(self, client):
-        f_name = os.path.join(os.path.dirname(__file__), 'simpleplane.urdf')
-        self.plane=p.loadURDF(fileName='./Multiagent-Reaction/multiagent_reaction/resources/simplegoal.urdf',
+        #f_name = os.path.join(os.path.dirname(__file__), 'simpleplane.urdf')
+        self.plane=p.loadURDF(fileName='./Multiagent-Reaction/multiagent_reaction/resources/simpleplane.urdf',
                             basePosition=[0, 0, 0],
                             physicsClientId=client)
     
